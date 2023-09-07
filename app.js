837dd2dcd6bd742ec550c8479fb16a7a5cf260a0
harsh-theexponent <81282949+harsh-theexponent@users.noreply.github.com>
2023-09-07 20:09:00 +0530

    res and req completed

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const http = require('http');
 
 const server=http.createServer((req,res)=>{
-    console.log(req);
+    // console.log(req);
 
-    process.exit()
+    console.log('url',req.url)
+    console.log('header',req.headers)
+    console.log('method',req.method)
+
+    res.setHeader('Content-Type','text/html')
+    res.write('<html>')
+    res.write('<head>My Node js</head>')
+    res.write('<body><h1>Nods js</h1></body>')
+    res.write('</html>')
+    res.end()
+
+    // process.exit()
     // Close the program
 })
 
