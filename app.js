58411b8b79ed8b2a7737c48ac91e8b250ef9ec31
harsh-theexponent <81282949+harsh-theexponent@users.noreply.github.com>
2023-11-20 23:53:31 +0530

    form-create

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 const http = require('http');
 
 const server=http.createServer((req,res)=>{
-    // console.log(req);
-
-    console.log('url',req.url)
-    console.log('header',req.headers)
-    console.log('method',req.method)
-
-    res.setHeader('Content-Type','text/html')
+   const url=req.url;
+   if(url==='/'){
     res.write('<html>')
-    res.write('<head>My Node js</head>')
-    res.write('<body><h1>Nods js</h1></body>')
+    res.write('<head>Form</head>')
+    res.write(`<body>
+    <form action="/message" method="POST">
+    <input type="text" name="msg"></input>
+    <button type="submit">Send</button>
+    </form>
+    </body>`)
     res.write('</html>')
-    res.end()
+    return res.end()
+   }
+
+    // res.setHeader('Content-Type','text/html')
+    // res.write('<html>')
+    // res.write('<head>My Node js</head>')
+    // res.write('<body><h1>Nods js</h1></body>')
+    // res.write('</html>')
+    // res.end()
 
     // process.exit()
     // Close the program
