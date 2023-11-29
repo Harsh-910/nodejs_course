af343825a99418fb967f2aa616daf09dfe97a5fd
harsh-theexponent <81282949+harsh-theexponent@users.noreply.github.com>
2023-11-29 19:49:05 +0530

    form-send

--- app.js
+++ app.js
@@ -1,29 +1,49 @@
-const http = require('http');
+const http = require("http");
+const fs = require("fs");
 
-const server=http.createServer((req,res)=>{
-   const url=req.url;
-   if(url==='/'){
-    res.write('<html>')
-    res.write('<head>Form</head>')
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head>Form</head>");
     res.write(`<body>
     <form action="/message" method="POST">
     <input type="text" name="msg"></input>
     <button type="submit">Send</button>
     </form>
-    </body>`)
-    res.write('</html>')
-    return res.end()
-   }
+    </body>`);
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const bodyParse = Buffer.concat(body).toString();
+      console.log(bodyParse);
+      const message = bodyParse.split("=");
+      console.log('message',message)
+      fs.writeFileSync("message.txt", message);
+    });
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
 
-    // res.setHeader('Content-Type','text/html')
-    // res.write('<html>')
-    // res.write('<head>My Node js</head>')
-    // res.write('<body><h1>Nods js</h1></body>')
-    // res.write('</html>')
-    // res.end()
+  // res.setHeader('Content-Type','text/html')
+  // res.write('<html>')
+  // res.write('<head>My Node js</head>')
+  // res.write('<body><h1>Nods js</h1></body>')
+  // res.write('</html>')
+  // res.end()
 
-    // process.exit()
-    // Close the program
-})
+  // process.exit()
+  // Close the program
+});
 
 server.listen(9000);
