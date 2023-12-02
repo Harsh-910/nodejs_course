ec59c9d0e42cbbce550dc2d7b15fa5b7aabf872c
harsh-theexponent <81282949+harsh-theexponent@users.noreply.github.com>
2023-12-02 23:16:03 +0530

    routing-setup-completed

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express=require("express")
 
 const app=express();
 
-app.use((req,res,next)=>{
-    console.log('In the middleware!')
-    next()   // allow the request to continue to the next middleware
+app.use('/add-product',(req,res,next)=>{
+    console.log('Product Page!')
+    res.send('<h1>Product Page</h1>')
 })
 
-app.use((req,res,next)=>{
+app.use('/',(req,res,next)=>{
     console.log('In the another middleware!')
     res.send('<h1>Hello From Express</h1>')
 })
