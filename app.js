5f66b9e88e71d8081c03ec3cc5465c6a7d082656
harsh-theexponent <81282949+harsh-theexponent@users.noreply.github.com>
2023-12-04 23:34:24 +0530

    routes-completed

--- app.js
+++ app.js
@@ -1,22 +1,15 @@
 const express=require("express")
 const bodyParser=require("body-parser")
 
+const adminRoutes=require('./Routes/admin')
+const shopRoutes=require('./Routes/shop')
+
 const app=express();
 
 app.use(bodyParser.urlencoded({extended:false}))
 
-app.use('/add-product',(req,res,next)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="title" /><button>Add Product</button></form>')
-})
+app.use(adminRoutes)
+app.use(shopRoutes)
 
-app.post('/product',(req,res,next)=>{
-    console.log(req.body)
-    res.redirect('/');
-})
-
-app.use('/',(req,res,next)=>{
-    console.log('In the another middleware!')
-    res.send('<h1>Hello From Express</h1>')
-})
 
 app.listen(9000)
