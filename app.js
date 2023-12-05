7035e75ebd1b56287412289adc1bd4c41b4c2a99
harsh-theexponent <81282949+harsh-theexponent@users.noreply.github.com>
2023-12-05 23:30:44 +0530

    path-util-added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express=require("express")
 const bodyParser=require("body-parser")
 const path=require("path")
+const rootDir=require("./util/path")
 
 const adminRoutes=require('./Routes/admin')
 const shopRoutes=require('./Routes/shop')
@@ -13,7 +14,7 @@ app.use('/admin',adminRoutes)
 app.use(shopRoutes)
 
 app.use((req,res,next)=>{
-    res.status(404).sendFile(path.join(__dirname,'./','views','404.html'))
+    res.status(404).sendFile(path.join(rootDir,'views','404.html'))
 })
 
 app.listen(9000)
