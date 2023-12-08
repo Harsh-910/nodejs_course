e42affafdd927e7794acc20483f86d7de1636454
harsh-theexponent <81282949+harsh-theexponent@users.noreply.github.com>
2023-12-08 23:04:09 +0530

    assignment-completed

--- app.js
+++ app.js
@@ -3,17 +3,19 @@ const bodyParser=require("body-parser")
 const path=require("path")
 const rootDir=require("./util/path")
 
-const adminRoutes=require('./Routes/admin')
-const shopRoutes=require('./Routes/shop')
-
 const app=express();
 
 app.use(bodyParser.urlencoded({extended:false}))
 
 app.use(express.static(path.join(__dirname,'public')))
 
-app.use('/admin',adminRoutes)
-app.use(shopRoutes)
+app.get('/user',(req,res,next)=>{
+    res.sendFile(path.join(__dirname,'./','Views','user.html'))
+})
+
+app.get('/',(req,res,next)=>{
+    res.sendFile(path.join(__dirname,'./','Views','home.html'))
+})
 
 app.use((req,res,next)=>{
     res.status(404).sendFile(path.join(rootDir,'views','404.html'))
