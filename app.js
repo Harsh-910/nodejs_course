76025cebf77b63d2b9ae7d1e78698d4a91e910f1
harsh-theexponent <81282949+harsh-theexponent@users.noreply.github.com>
2023-12-14 23:38:57 +0530

    ejs completed

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 const express=require("express")
 const bodyParser=require("body-parser")
 const path=require("path")
-const hbs=require("express-handlebars")
 
 const adminData=require('./Routes/admin')
 const shopRoutes=require('./Routes/shop')
 
 const app=express();
 
-app.engine('hbs',hbs({layoutsDir:'views/layouts',defaultLayout:'main-layout',extname:'hbs'}))
-app.set('view engine','hbs')
+app.set('view engine','ejs')
 app.set('views', 'views')
 
 app.use(bodyParser.urlencoded({extended:false}))
